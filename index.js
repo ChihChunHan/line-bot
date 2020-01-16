879910ae2f3588b58acdafb918a9729b0c689a68
ChihChunHan <57065101+ChihChunHan@users.noreply.github.com>
2020-01-16 11:56:33 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -19,12 +19,10 @@ bot.on('message', event => {
     rp('https://data.ntpc.gov.tw/api/v1/rest/datastore/382000000A-000352-001')
       .then(htmlString => {
         let json = JSON.parse(htmlString)
-        console.log(json)
         json = json.result.records.filter(j => {
           if (j.sna === usermsg) return true
           else return false
         })
-        console.log(json)
         if (json.length > 0) event.reply('可借車數量' + json[0].sbi)
         else event.reply('沒有資料')
       })
